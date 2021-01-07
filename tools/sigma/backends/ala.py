debc17cc6c0175b0e8fde3df37f6461d20496a48
Luke Fritz <lukefritz@gmail.com>
2021-01-07 17:16:06 -0600

    Add tests for known KQL syntax errors; adjust ala backend

--- tools/sigma/backends/ala.py
+++ tools/sigma/backends/ala.py
@@ -106,32 +106,40 @@ class AzureLogAnalyticsBackend(SingleTextQueryBackend):
         return parse_arg
 
     def default_value_mapping(self, val):
-        op = "=="
-        if isinstance(val, str):
-            if "*" in val[1:-1]:  # value contains * inside string - use regex match
-                op = "matches regex"
-                val = re.sub('(\\\\\*|\*)', '.*', val)
-                if "\\" in val:
-                    val = "@'(?i)%s'" % (val)
-                else:
-                    val = "'(?i)%s'" % (val)
-                return "%s %s" % (op, self.cleanValue(val))
-            elif val.startswith("*") or val.endswith("*"):
-                if val.startswith("*") and val.endswith("*"):
-                    op = "contains"
-                elif val.startswith("*"):
-                    op = "endswith"
-                elif val.endswith("*"):
-                    op = "startswith"
-                val = re.sub('([".^$]|(?![*?]))', '\g<1>', val)
-                val = re.sub('(\\\\\*|\*)', '.*', val)
-                val = re.sub('\\?', '.', val)
-                if "\\" in val:
-                    return "%s @'%s'" % (op, self.cleanValue(val))
-                return "%s '%s'" % (op, self.cleanValue(val))
-            elif "\\" in val:
-                return "%s @'%s'" % (op, self.cleanValue(val))
-        return "%s \"%s\"" % (op, self.cleanValue(val))
+        try:
+            # Check whether the input is a valid regex. If this raises an error we know we can
+            # treat it as a normal value. Otherwise, we'll assume it's a regex unless it matches
+            # common conditions that shouldn't be regexes.
+            re.compile(val)
+
+            # If the value doesn't contain any special characters it shouldn't need to be a regex
+            if re.match(r'[^\w\s]', val) is None:
+                return self.non_regex_value_mapping(val)
+
+            # `abc*` is a valid regexp but the language spec treats this as `endswith`
+            if re.match(r'.*[^\.]\*$', val) is not None:
+                return self.non_regex_value_mapping(val)
+
+            # TODO: There are probably more scenarios we need to account for here...
+
+            return f'matches regex @"{val}"'
+        except:
+            return self.non_regex_value_mapping(val)
+
+    def non_regex_value_mapping(self, val):
+        op = '=='
+        if val.startswith('*'):
+            if val.endswith('*'):
+                op = 'contains'
+            else:
+                op = 'endswith'
+        elif val.endswith('*'):
+            op = 'startswith'
+
+        val = re.sub(r'(^\*|\*$)', '', val)
+
+        return f'{op} "{val}"'
+
 
     def getTable(self, sigmaparser):
         if self.category == "process_creation" and len(set(sigmaparser.values.keys()) - {"Image", "ParentImage",
---
+++ tools/tests/test_backend_ala.py
@@ -0,0 +1,61 @@
+"""Tests for the Azure Log Analytics backend."""
+
+import pytest
+
+from sigma.backends.ala import AzureLogAnalyticsBackend
+from sigma.configuration import SigmaConfiguration
+
+
+@pytest.fixture()
+def sigmaconfig():
+    """Return a new instance of the sigmaconfig."""
+    with open('../tools/config/ala.yml') as f:
+        return SigmaConfiguration(f)
+
+
+@pytest.fixture()
+def backend(sigmaconfig):
+    """Return a new instance of the backend."""
+    return AzureLogAnalyticsBackend(sigmaconfig)
+
+
+def test_default_value_mapping_simple_contains(backend):
+    """Should return a simple `contains` KQL condition."""
+    val = '*.dll*'
+    result = backend.default_value_mapping(val)
+    assert result == 'contains ".dll"'
+
+
+def test_default_value_mapping_preserve_slashes(backend):
+    """Should preserve slashes in directory paths."""
+    val = "*/.git/*"
+    result = backend.default_value_mapping(val)
+    assert result == 'contains "/.git/"'
+
+
+def test_default_value_mapping_starts_with(backend):
+    """Should return a simple `startswith` KQL condition."""
+    val = 'abc*'
+    result = backend.default_value_mapping(val)
+    assert result == 'startswith "abc"'
+
+
+def test_default_value_mapping_ends_with(backend):
+    """Should return a simple `endswith` KQL condition."""
+    val = '*.env'
+    result = backend.default_value_mapping(val)
+    assert result == 'endswith ".env"'
+
+
+def test_default_value_mapping_regex_prefix(backend):
+    """Should prefix regex strings with `@`."""
+    val = '.*test.*'
+    result = backend.default_value_mapping(val)
+    assert result == 'matches regex @".*test.*"'
+
+
+def test_default_value_mapping_simple_equals(backend):
+    """Should return a simple `==` KQL condition."""
+    val = 'test'
+    result = backend.default_value_mapping(val)
+    assert result == '== "test"'
